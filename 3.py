8344d5319065867bb006e67ed6bdc1a527fd7eab
Sam Vanderstraeten <sam.vanderstraeten@pxl.be>
2022-12-04 14:53:09 +0100

    III. BACKPACKIN'

--- 3.py
+++ 3.py
@@ -1,12 +1,14 @@
-from util.parser import Parser
-from util.printer import Printer
-
-file = open("test/3.sam", "r")
-#file = open("input/1.sam", "r")
+file = open("input/3.sam", "r")
 lines = file.readlines()
 
-#data = [int(n) for n in lines]
-#data = [x.split(",") for x in lines]
-#grid = Parser.parse_grid(lines)
-#Printer.print_grid(grid)
+def get_score(c):
+    return ord(c)-ord('A')+27 if c.isupper() else ord(c)-ord('a')+1
 
+score = sum([get_score([d for d in line[int(len(line)/2):] if d in line[0:int(len(line)/2)]][0]) for line in lines])
+print("Part I: ",score)
+
+pr = 0
+for i in range(int(len(lines)/3)):
+    a,b,c = lines[i*3:i*3+3]
+    pr += get_score([w for w in a if w in b and w in c][0])
+print("Part II:",pr)
