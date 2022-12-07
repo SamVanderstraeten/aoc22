734d382869c27345394fa3fd6fca98871e978d67
Sam Vanderstraeten <sam.vanderstraeten@pxl.be>
2022-12-07 09:11:15 +0100

    VII. TERMINALTOR

--- 7.py
+++ 7.py
@@ -1,12 +1,51 @@
-from util.parser import Parser
-from util.printer import Printer
+file = open("input/7.sam", "r")
+lines = [line.strip() for line in file.readlines()]
 
-file = open("test/7.sam", "r")
-#file = open("input/1.sam", "r")
-lines = file.readlines()
+counts = {}
+dirs = {}
+cwd = []
 
-#data = [int(n) for n in lines]
-#data = [x.split(",") for x in lines]
-#grid = Parser.parse_grid(lines)
-#Printer.print_grid(grid)
+def dir_size(dir):
+    size = counts[dir]
+    if dir in dirs:
+        for sub in dirs[dir]:
+            size += dir_size(dir+sub)
+    return size
 
+for line in lines:
+    if line[0] == "$": #command
+        cmd = line[2:4]
+        if cmd == "cd":
+            dir = line[5:]
+            if dir == "/":
+                cwd = ["/"]
+            elif dir == "..":
+                cwd.pop()
+            else:
+                cwd.append(dir)
+    else:
+        c = "".join(cwd)
+        if not c in counts:
+            counts[c] = 0
+        if not c in dirs:
+            dirs[c] = []   
+        
+        (a, b) = line.split(" ")
+        if a == "dir":    
+            dirs[c].append(b)
+        else:            
+            counts[c] += int(a)
+
+sizes = []
+sum = 0
+for c in counts:
+    size = dir_size(c)
+    if size <= 100000:
+        sum += size
+    sizes.append(size)
+print("Part I: ",sum) 
+
+needed_space = dir_size("/") - 40000000
+sizes.sort()
+delete_folder = [s for s in sizes.sort() if s>needed_space].pop(0)
+print("Part II:", delete_folder)
