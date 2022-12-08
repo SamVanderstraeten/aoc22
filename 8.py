769373343d234e824b059de4e507cf297568aeb9
Sam Vanderstraeten <sam.vanderstraeten@pxl.be>
2022-12-08 09:35:17 +0100

    VIII. TREE SEARCH

--- 8.py
+++ 8.py
@@ -23,6 +23,7 @@ for i in range(1, len(grid)-1):
     line = grid[i]
     v.append(check_line(line, i, -1))
     v.append(check_line(line[::-1], i, -1, True))
+    
 for i in range(1, len(grid[0])-1):
     line = [a[i] for a in grid]
     v.append(check_line(line, -1, i))
