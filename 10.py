4df6b3463cb2e6f38b79d87689417acf8a948d4c
Sam Vanderstraeten <sam.vanderstraeten@pxl.be>
2022-12-09 09:02:03 +0100

    IX. THIS WAS FUN [PAUSE] KNOT

---
+++ 10.py
@@ -0,0 +1,12 @@
+from util.parser import Parser
+from util.printer import Printer
+
+file = open("test/10.sam", "r")
+#file = open("input/1.sam", "r")
+lines = file.readlines()
+
+#data = [int(n) for n in lines]
+#data = [x.split(",") for x in lines]
+#grid = Parser.parse_grid(lines)
+#Printer.print_grid(grid)
+
--- 9.py
+++ 9.py
@@ -1,12 +1,31 @@
-from util.parser import Parser
-from util.printer import Printer
+file = open("input/9.sam", "r")
+lines = [l.strip() for l in file.readlines()]
 
-file = open("test/9.sam", "r")
-#file = open("input/1.sam", "r")
-lines = file.readlines()
+def zero_or_abs(n):
+    return 0 if n == 0 else n/abs(n)
 
-#data = [int(n) for n in lines]
-#data = [x.split(",") for x in lines]
-#grid = Parser.parse_grid(lines)
-#Printer.print_grid(grid)
+def follow(knot, target):
+    dx = target[0] - knot[0]
+    dy = target[1] - knot[1]    
+    if abs(dx)<=1 and abs(dy)<=1:
+        return knot
+    return (knot[0] + zero_or_abs(dx), knot[1] + zero_or_abs(dy))
 
+def move(num):
+    dirs = {'R': (1,0), 'L': (-1,0), 'D': (0,-1), 'U': (0,1)}
+    posT = []
+    head = (0,0)
+    knots = [(0,0)]*num
+    for line in lines:
+        d, num = line.split(" ")
+        for i in range(int(num)):
+            head = (head[0]+dirs[d][0], head[1]+dirs[d][1])
+            prev = head
+            for k in range(len(knots)):
+                knots[k] = follow(knots[k], prev)
+                prev = knots[k]
+            posT.append(knots[-1])
+    return len(set(posT))
+
+print("Part I: ", move(1))
+print("Part II:", move(9))
