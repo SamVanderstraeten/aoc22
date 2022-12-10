b786d042d6d51b0d10c03c6308d0085b59d717a2
Sam Vanderstraeten <sam.vanderstraeten@pxl.be>
2022-12-10 15:42:39 +0100

    X. LIKE CLOCKWORK

--- 10.py
+++ 10.py
@@ -1,12 +1,40 @@
-from util.parser import Parser
-from util.printer import Printer
+file = open("input/10.sam", "r")
+lines = [line.strip() for line in file.readlines()]
 
-file = open("test/10.sam", "r")
-#file = open("input/1.sam", "r")
-lines = file.readlines()
+x = 1
+cycle = 0
+s = []
+crt = []
 
-#data = [int(n) for n in lines]
-#data = [x.split(",") for x in lines]
-#grid = Parser.parse_grid(lines)
-#Printer.print_grid(grid)
+def parse_instruction(line):
+    if line[:4] == "noop":
+        return "noop", 0
+    else:
+        return line.split(" ")
 
+def get_num_cycles(instr):
+    return 1 if instr == "noop" else 2
+
+def get_signal_strength():
+    return x*cycle
+
+for line in lines:
+    instr, arg = parse_instruction(line)
+    cycles = get_num_cycles(instr)
+    for i in range(cycles):
+        if (x-1) <= cycle%40 <= (x+1): # scanlines are each [0,40]... seems I skipped that part
+            crt.append("#")
+        else:
+            crt.append(" ")
+        cycle += 1
+        if cycle%20 == 0 and (cycle/20)%2==1: # 20, 60, 100, 140, 180, 220
+            s.append(get_signal_strength())       
+    if instr == "addx":
+        x += int(arg)
+
+print("Part I: ",sum(s))
+print("Part II:")
+for i in range(6):
+    for p in crt[i*40:(i+1)*40]:
+        print(p,end='')
+    print('')
